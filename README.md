422600a8496aa92673b74a31fb943fb5acb9307e
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-10-27 11:51:57 +0100

    Basic build system, unit-testing, and Doxygen documentation.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # profiler
 Profiler for scientific code on HPC platforms.
+
+## Building
+Requires a c++17 compatible compiler, currently only tested with GCC-9.2.0.
+
+The testing framework is GoogleTest (1.11.0) and source code documentation is created using Doxygen (1.8.5).
+
+**To build:**  
+In project root directory.
+~~~~~~~~~~~~~~~~shell
+mkdir build
+cd build
+cmake ..
+make
+make test
+~~~~~~~~~~~~~~~~
---
+++ tests/hello_test.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*\
+ (C) Crown copyright 2021 Met Office. All rights reserved.
+ The file LICENCE, distributed with this code, contains details of the terms
+ under which the code may be used.
+--------------------------------------------------------------------------------
+ Description
+ A basic unit test example using GTest
+\*----------------------------------------------------------------------------*/
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
