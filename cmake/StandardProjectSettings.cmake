75aa036ab774d225cdbecb827ef90d5e6f2bd3a4
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-11-05 16:05:23 +0000

    Code review changes to cmake/StandardProjectSettings.cmake

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,3 +1,6 @@
+# Some basic project settings for defaults such as build type and installation
+# prefix
+
 # Set C++ standard compliance
 set(CMAKE_CXX_STANDARD 17)
 
@@ -7,20 +10,24 @@ if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
     set(CMAKE_BUILD_TYPE
             RelWithDebInfo
             CACHE STRING "Choose the type of build." FORCE)
-    # Set the possible values of build type for cmake-gui, ccmake
+    # Set the possible values of build type for cmake-gui, ccmake.
     set_property(
             CACHE CMAKE_BUILD_TYPE
             PROPERTY STRINGS
-            "Debug"
-            "Debug-Coverage"
-            "Release"
-            "MinSizeRel"
-            "RelWithDebInfo")
+            # These build type options set the listed compiler options.
+            # These are CMake default flags for C++
+            "Debug" # -g
+            "Release" # -O3 -DNDEBUG
+            "MinSizeRel" # -Os -DNDEBUG
+            "RelWithDebInfo" # -O2 -g -DNDEBUG
+    )
 endif ()
 
-# Generate compile_commands.json to make it easier to work with clang based tools
+# Generate compile_commands.json to make it easier to work with Clang-based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
+# Interprocedural optimisation is useful for improving program performance through
+# analysis of the entire program rather than just single program units.
 option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
 
 if (ENABLE_IPO)
@@ -37,6 +44,7 @@ if (ENABLE_IPO)
     endif ()
 endif ()
 
+#Set default install location to be in build directory rather than /usr/bin etc.
 if(${CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT})
     set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE PATH "..." FORCE)
 endif()
