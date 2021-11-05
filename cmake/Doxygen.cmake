d129a4df9615d9e13935351ad5b15f95f2b87a4f
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-11-05 15:12:14 +0000

    Code review changes to cmake/Doxygen.cmake

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,3 +1,6 @@
+# This file sets up the options for the Doxygen generation of API documentation
+# available options for CMake Doxygen integration can be found here:
+# https://cmake.org/cmake/help/latest/module/FindDoxygen.html
 function(enable_doxygen)
     option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" ON)
     if (ENABLE_DOXYGEN)
@@ -7,8 +10,9 @@ function(enable_doxygen)
         set(DOXYGEN_PRIVATE YES)
         set(DOXYGEN_GENERATE_LATEX YES)
         set(DOXYGEN_LATEX_OUTPUT pdf)
+        set(DOXYGEN_MARKDOWN_SUPPORT YES)
         set(DOXYGEN_HIDE_SCOPE_NAMES YES)
-        set(DOXYGEN_PROJECT_NAME "Profiler")
+        set(DOXYGEN_PROJECT_NAME "${CMAKE_PROJECT_NAME}")
         set(DOXYGEN_PROJECT_NUMBER "v${VERSION}")
         set(DOXYGEN_DISABLE_INDEX NO)
         set(DOXYGEN_GENERATE_TREEVIEW YES)
@@ -19,9 +23,14 @@ function(enable_doxygen)
         set(DOXYGEN_STRIP_CODE_COMMENTS NO)
         set(DOXYGEN_HTML_HEADER ${PROJECT_SOURCE_DIR}/documentation/Doxygen/html/header.html)
         set(DOXYGEN_HTML_FOOTER ${PROJECT_SOURCE_DIR}/documentation/Doxygen/html/footer.html)
-#        set(DOXYGEN_HTML_STYLESHEET ${PROJECT_SOURCE_DIR}/documentation/Doxygen/customdoxygen.css)
+
+        # Find the Doxygen application install (requires dot)
         find_package(Doxygen REQUIRED dot)
+
+        # Set up Doxygen build with source directory and additional pages.
         doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/src ${PROJECT_SOURCE_DIR}/documentation/Doxygen ALL)
+
+        # Set up installation destination
         install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/docs)
     endif ()
 endfunction()
