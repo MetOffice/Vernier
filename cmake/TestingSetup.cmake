3c3468c66dd36ebc4addabd466c59df5891fd6a1
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-12-02 16:38:29 +0000

    Issue #3 - Added the option to fetch googletest and build in-situ for testing.

--- cmake/TestingSetup.cmake
+++ cmake/TestingSetup.cmake
@@ -1,10 +1,20 @@
-# Enable CTest integration with GoogleTest and add a directory for the tests
+option(INCLUDE_GTEST "Fetch GoogleTest framework during build" OFF)
 
-# Find required testing packages
-find_package(GTest REQUIRED)
+if (INCLUDE_GTEST)
+    message(STATUS "Fetching GoogleTest")
+    include(FetchContent)
+    FetchContent_Declare(
+            googletest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG	e2239ee6043f73722e7aa812a459f54a28552929 # release-1.11.0
+    )
+    FetchContent_MakeAvailable(googletest)
+    set(TESTING_LIB gtest_main)
+else()
+    message(STATUS "Using local GoogleTest installation")
+    find_package(GTest REQUIRED)
+    set(TESTING_LIB GTest::Main)
+endif ()
 include(GoogleTest)
-
-# Enable CTest
 enable_testing()
-
 add_subdirectory(tests)
