9f7aa35588db510b04cc085847036f8b17cd63c4
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-12-03 11:58:20 +0000

    Changed so on project install it doesn't install googletest with it. Also added some more comments.

--- cmake/TestingSetup.cmake
+++ cmake/TestingSetup.cmake
@@ -1,20 +1,47 @@
+# This sets up the testing framework inclusion for the project, optionally
+# googletest can be downloaded from the git repository and built in the main
+# build directory for use by ctest.
+
+# Add optional argument to fetch googletest
 option(INCLUDE_GTEST "Fetch GoogleTest framework during build" OFF)
 
 if (INCLUDE_GTEST)
     message(STATUS "Fetching GoogleTest")
+
+    # Include FetchContent module to enable populating content at configure time.
     include(FetchContent)
+
+    # Set up googletest options to populate lib.
     FetchContent_Declare(
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG	e2239ee6043f73722e7aa812a459f54a28552929 # release-1.11.0
+            GIT_TAG	e2239ee6043f73722e7aa812a459f54a28552929 # hash of release-1.11.0.
     )
-    FetchContent_MakeAvailable(googletest)
-    set(TESTING_LIB gtest_main)
+
+    # Get the properties of the content being populated (e.g. googletest_BINARY_DIR).
+    FetchContent_GetProperties(googletest)
+
+    # Populate content but don't allow installation with project library.
+    if(NOT googletest_POPULATED)
+        FetchContent_Populate(googletest)
+        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
+
+    # Set name of test lib for use with target_link_libraries.
+    set(GTEST_MAIN_NAME gtest_main)
 else()
     message(STATUS "Using local GoogleTest installation")
+
+    # Find installed googletest library.
     find_package(GTest REQUIRED)
-    set(TESTING_LIB GTest::Main)
+
+    # Set name of test lib for use with target_link_libraries.
+    set(GTEST_MAIN_NAME GTest::Main)
 endif ()
+
+# Include googletest testing library in project.
 include(GoogleTest)
+
+# Enable testing with CTest
 enable_testing()
 add_subdirectory(tests)
