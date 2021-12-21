083c3b76020a85c2f0dcfd751513a4180375e95e
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-12-21 13:02:03 +0000

    Moving installation commands to cmake/Installation.cmake  (#22)
    * Change versioning method in CMake to use the project versioning format rather than user defined variables.
    
    * Added comment for project command
    
    * Moved installation commands to cmake/Installation.cmake [skip ci]
    
    * Updated to deal with PR #19 merge [skip ci]
    
    * PR #22: Updated changelog for merge to main
    
    Co-authored-by: TeranIvy <TeranIvy@users.noreply.github.com>

---
+++ cmake/Installation.cmake
@@ -0,0 +1,29 @@
+# Set up the installation and creation of *.cmake files for use with the
+# find_package cmake function.
+install(
+        EXPORT "${CMAKE_PROJECT_NAME}Targets"
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${CMAKE_PROJECT_NAME}
+        NAMESPACE   ${CMAKE_PROJECT_NAME}::
+        FILE       "${CMAKE_PROJECT_NAME}Targets.cmake"
+)
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(
+        "${CMAKE_PROJECT_NAME}Config.cmake.in"
+        "${CMAKE_PROJECT_NAME}Config.cmake"
+        INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${CMAKE_PROJECT_NAME}"
+        PATH_VARS CMAKE_INSTALL_LIBDIR
+)
+
+write_basic_package_version_file(
+        "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}ConfigVersion.cmake"
+        VERSION ${PROJECT_VERSION}
+        COMPATIBILITY SameMajorVersion
+)
+
+install(
+        FILES "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}Config.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}ConfigVersion.cmake"
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${CMAKE_PROJECT_NAME}"
+)
