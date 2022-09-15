d96c031f4ed5ebeba00806d2b8092514a51321ca
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2022-09-15 09:21:13 +0100

    Reformatted the copyright statements in all the source files to make them all the same style, and added where they are missing.

--- tests/unit_tests/c++/test_hashtiming.cpp
+++ tests/unit_tests/c++/test_hashtiming.cpp
@@ -1,3 +1,9 @@
+/*----------------------------------------------------------------------------*\
+ (c) Crown copyright 2022 Met Office. All rights reserved.
+ The file LICENCE, distributed with this code, contains details of the terms
+ under which the code may be used.
+\*----------------------------------------------------------------------------*/
+
 #include <iostream>
 #include <chrono>
 #include <profiler.h>
