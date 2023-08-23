339e75d3cfcf8859b65d450f44e5418d98499a1a
Maff Glover <78152252+mo-mglover@users.noreply.github.com>
2023-08-23 16:54:44 +0100

    Add direct recursion test. Slacken time tolerance.

--- tests/unit_tests/c++/test_recursion.cpp
+++ tests/unit_tests/c++/test_recursion.cpp
@@ -9,19 +9,27 @@
 
 #include "vernier.h"
 
-int const max_depth = 3;
-int const sleep_seconds = 1;
+int const    max_depth = 3;
+int const    sleep_seconds = 1;
+
+// The time tolerance can be reasonably loose. If the total times were incorrect
+// as a result of mis-handled recursion, they would be too large by multiples of
+// sleep_seconds.
+double const time_tolerance = 0.01;
 
 // ------------------------------------------------------------------------------
 //  File scope variables
 // ------------------------------------------------------------------------------
 
+// Structure To hold timings from the various recursive functions.
 struct Timings
 {
-  static double first_function_total_time ;
+  static double zeroth_function_total_time;
+  static double first_function_total_time;
   static double second_function_total_time;
 };
 
+double Timings::zeroth_function_total_time = 0.0;
 double Timings::first_function_total_time  = 0.0;
 double Timings::second_function_total_time = 0.0;
 
@@ -29,11 +37,40 @@ double Timings::second_function_total_time = 0.0;
 //  Forward declarations
 // ------------------------------------------------------------------------------
 
-void first_function(Timings&);
+void zeroth_function(Timings&);
+void first_function (Timings&);
 void second_function(Timings&);
 
 // ------------------------------------------------------------------------------
-//  First test function
+//  Zeroth function - calls itself
+// ------------------------------------------------------------------------------
+
+void zeroth_function(Timings& timings)
+{
+
+  static int recursion_depth = 1;
+#pragma omp threadprivate(recursion_depth)
+
+  auto prof_handle = meto::vernier.start("first_function");
+
+  sleep(sleep_seconds);
+  ++recursion_depth;
+
+  if (recursion_depth <= max_depth)
+  {
+    zeroth_function(timings);
+  }
+
+  meto::vernier.stop(prof_handle);
+
+  // Update the total walltime so far spent in this function.
+  int const tid = omp_get_thread_num();
+  timings.zeroth_function_total_time = meto::vernier.get_total_walltime(prof_handle, tid);
+
+}
+
+// ------------------------------------------------------------------------------
+//  First function - calls the second function.
 // ------------------------------------------------------------------------------
 
 void first_function(Timings& timings)
@@ -54,13 +91,14 @@ void first_function(Timings& timings)
 
   meto::vernier.stop(prof_handle);
 
+  // Update the total walltime so far spent in this function.
   int const tid = omp_get_thread_num();
   timings.first_function_total_time = meto::vernier.get_total_walltime(prof_handle, tid);
 
 }
 
 // ------------------------------------------------------------------------------
-//  Second test function
+//  Second function - calls the first function.
 // ------------------------------------------------------------------------------
 
 void second_function(Timings& timings)
@@ -85,25 +123,61 @@ void second_function(Timings& timings)
 }
 
 // -------------------------------------------------------------------------------
-//  Main test
+//  Main tests
 // -------------------------------------------------------------------------------
 
-TEST(RecursionTest,IndirectRecursion)
+//
+// Direct recursion
+//
+
+TEST(RecursionTest,DirectRecursion)
 {
   auto prof_handle = meto::vernier.start("test_recursion");
 
-  // Test indepedently on each thread.
+  // Test independently on each thread.
 #pragma omp parallel
   {
     Timings timings;
     double t1 = omp_get_wtime();
 
     auto prof_handle_threaded = meto::vernier.start("test_recursion:threads");
+
+    // Function calls itself
+    zeroth_function(timings);
+
+    double t2 = omp_get_wtime();
+    double overall_time = t2-t1;
+
+    EXPECT_LE  (timings.zeroth_function_total_time,  overall_time);
+    EXPECT_NEAR(timings.zeroth_function_total_time,  overall_time, time_tolerance);
+
+    meto::vernier.stop(prof_handle_threaded);
+  }
+
+  meto::vernier.stop(prof_handle);
+}
+
+//
+// Indirect recursion
+//
+
+TEST(RecursionTest,IndirectRecursion)
+{
+  auto prof_handle = meto::vernier.start("test_recursion");
+
+  // Test independently on each thread.
+#pragma omp parallel
+  {
+    Timings timings;
+    double t1 = omp_get_wtime();
+
+    auto prof_handle_threaded = meto::vernier.start("test_recursion:threads");
+
+    // Function calls a second function
     first_function(timings);
 
     double t2 = omp_get_wtime();
     double overall_time = t2-t1;
-    double constexpr time_tolerance = 0.0005;
     
     EXPECT_LE  (timings.first_function_total_time,  overall_time);
     EXPECT_NEAR(timings.first_function_total_time,  overall_time, time_tolerance);
