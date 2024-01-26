cf96ddee0e3db4c153098754de08376abbde4c62
Dylan13591 <138667492+Dylan13591@users.noreply.github.com>
2024-01-26 15:33:27 +0000

    121 run without openmp (#127)
    In this issue I made it so Vernier could run without OpenMP by adding
    #ifdef calls around OpenMP code. I then created a new timing method,
    vernier_get_wtime(), which replaced all omp_get_wtime() calls. Lastly I
    added the option to build with/without OpenMP into the RunWithout.cmake
    file.

---
+++ src/c++/vernier_get_wtime.h
@@ -0,0 +1,24 @@
+/* -----------------------------------------------------------------------------
+ *  (c) Crown copyright 2022 Met Office. All rights reserved.
+ *  The file LICENCE, distributed with this code, contains details of the terms
+ *  under which the code may be used.
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * @file   vernier_get_wtime().h
+ * @brief  vernier_get_wtime function which returns the current time
+ *
+ * 
+ */
+
+#ifndef VERNIER_GET_WTIME_H
+#define VERNIER_GET_WTIME_H
+
+namespace meto
+{
+    // Function prototypes
+    double vernier_get_wtime();
+}
+
+#endif
