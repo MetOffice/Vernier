05ebaa4319626187caf994c450e993cf0f65dfc0
jedbakerMO <jed.baker@metoffice.gov.uk>
2025-04-23 09:39:48 +0100

    Editing script according to requests by Andie in #160

--- post-process.py
+++ post-process.py
@@ -7,35 +7,30 @@ import argparse
 from pathlib import Path
 import glob
 
-""" Global Variables """
-
-file_name = "vernier-output-"
 
 """ Main funcs """
 
 def parse_cli_arguments(arguments: list[str] = None):
 
     """ Parses command line arguments for running the script in the terminal """
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-path",          type=Path,  default=(os.getcwd()),                help="Path to Vernier output files")
     parser.add_argument("-outputname",    type=str,   default=str("vernier-merged-output"), help="Name of file to write to.")
+    parser.add_argument("-inputname",     type=str,   default=str("vernier-output-"),       help="Vernier files to read from.")
 
     return parser.parse_args(args=arguments)
 
-def read_mpi_ranks(current_dir_path):
+def read_mpi_ranks(current_dir_path, input_name):
 
     """ Reads the number of vernier-output files from the given directory to determine no. of ranks to use """
-
-    files = glob.glob(f"{current_dir_path}/{file_name}*")
+    files = glob.glob(f"{current_dir_path}/{input_name}*")
 
     return len(files)
 
-def read_and_pre_process(file_path, rank):
+def read_and_pre_process(file_path, rank, input_name):
 
     """ Reads in the current file """
-
-    file = open(f'{file_path}/{file_name}{rank}')
+    file = open(f'{file_path}/{input_name}{rank}')
     content = file.read()
 
     """ Removes information from beginning of file """
@@ -43,7 +38,6 @@ def read_and_pre_process(file_path, rank):
     content=content[del_index:]
 
     """ Create a new dataframe based on the pruned output file """
-
     header_names = ["index", "%Time", "Cumul", "Self", "Total", "Calls", "Se/cl", "To/cl", "Routine"]
     dataframe = pd.read_csv(sio(content), sep=r"\s+", engine="python", names=header_names)
 
@@ -54,36 +48,31 @@ def read_and_pre_process(file_path, rank):
 
     return temp_dataframe
 
-def merge_and_analyse(file_path, mpiranks):
+def merge_and_analyse(file_path, mpiranks, input_name):
 
     """ Reads in the files and performs analysis on them """
-
     print(f"Path to open: {file_path}")
     print(f"Detected {mpiranks} files.")
 
     for rank in range(0,mpiranks):
 
         """ Open the file, read it, workout where it actually starts """
-
-        dataframe = read_and_pre_process(file_path, rank)
+        dataframe = read_and_pre_process(file_path, rank, input_name)
 
         if rank == 0:     
 
             prev_df = dataframe.copy()
-
+            min_df = dataframe.copy()
+            max_df = dataframe.copy()
+                    
         else:
 
             """ Adds the new loaded dataframe to the previous one, resorting and reorganising the indices every time """
-
-            new_df = prev_df.add(dataframe)              
-            new_df["Routine"] = prev_df["Routine"]                  
-            new_df=new_df.sort_values(by="Routine")
-            new_df = new_df.reset_index(drop=True)
-
+            new_df = prev_df.add(dataframe)
+            new_df["Routine"] = prev_df["Routine"]           
             prev_df = new_df.copy()
                 
-    """ Averages the summed dataframe """
-        
+    """ Averages the summed dataframe """    
     mean_df = prev_df.drop(columns=["Routine"]) / int(mpiranks)
     mean_df["Routine"] = prev_df["Routine"]
 
@@ -94,8 +83,8 @@ def main():
     args = parse_cli_arguments()
     file_path = args.path
     merged_file_name = args.outputname
-
-    mpiranks = read_mpi_ranks(f'{file_path}')
+    input_name = args.inputname
+    mpiranks = read_mpi_ranks(file_path, input_name)
 
     if mpiranks == 0:
 
@@ -105,19 +94,16 @@ def main():
     else:
 
         print("\nReading and Merging...")
-        merged_frame = merge_and_analyse(file_path, int(mpiranks))
+
+        merged_frame = merge_and_analyse(file_path, int(mpiranks), input_name)
+
+
         print("\nWriting...")
         with open(f"{merged_file_name}", 'w') as f:
                   f.write(merged_frame.to_string(index=False, col_space=10))
+
         print(f"Merged outputs written to {merged_file_name}\n")
 
 
-main()
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
