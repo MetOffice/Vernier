83db7e8a1c3e9506da713c3de821e13cfa2ef6a5
Sam Clarke-Green <sam.clarke@metoffice.gov.uk>
2025-05-28 17:01:34 +0100

    Generate and install a pkg-config file for Vernier
    Add a template pc file, use cmake to fill in the variables, and
    install the the config in its final location when the library is
    installed.  The functionality is on by default, but it can be switched
    off by adding -DENABLE_PKGCONFIG=OFF to the cmake command line.

---
+++ cmake/PkgConfig.cmake
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  (c) Crown copyright 2025 Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# ------------------------------------------------------------------------------
+
+if(ENABLE_PKGCONFIG)
+  # Create and install a pkg-config file
+  message(STATUS "Generating a pkg-config file")
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/share/pkgconfig/${PROJECT_NAME}.pc.in
+                 ${CMAKE_CURRENT_BINARY_DIR}/share/pkgconfig/${PROJECT_NAME}.pc
+                 @ONLY)
+
+  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/share/pkgconfig/${PROJECT_NAME}.pc
+          DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/pkgconfig)
+endif()
