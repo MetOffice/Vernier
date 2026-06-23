d8707024d885e1f7aa9ff10687596a64397b4828
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2026-06-23 14:01:31 +0100

    Formatting

--- src/c++/formatter.cpp
+++ src/c++/formatter.cpp
@@ -44,9 +44,9 @@ meto::Formatter::Formatter() {
 /**
  * @brief  Executes the format_ method to write the data.
  *
- * @param[inout] header   Output stream for the format header
- * @param[inout] os       Output stream that the format method will write data to
- * @param[in]    hashvec  Vector of data that the format method will operate on
+ * @param[inout] header  Output stream for the format header
+ * @param[inout] os      Output stream for the format method will write data to
+ * @param[in]    hashvec Vector of data that the format method will operate on
  */
 
 void meto::Formatter::execute_format(std::ostream &header, std::ostream &os,
